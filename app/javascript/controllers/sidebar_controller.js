c8f013d03f09c4be95f6b1032cc91be765f95c33
Oskar Grabowski <oskar.grabowski@gmail.com>
2025-06-06 19:32:38 +0200

    Sideabar

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,3 +1,4 @@
+
 // app/javascript/controllers/sidebar_controller.js
 import { Controller } from "@hotwired/stimulus"
 
@@ -9,12 +10,48 @@ export default class extends Controller {
         this.isOpen = false
     }
 
+    connect() {
+        this.handleKeyDown = this.handleKeyDown.bind(this)
+        document.addEventListener('keydown', this.handleKeyDown)
+
+        // Przywróć ostatni stan sidebara z localStorage
+        const savedState = localStorage.getItem('sidebarOpen')
+        if (savedState === 'true') {
+            // Przywracamy stan bez animacji (sidebar już jest otwarty)
+            this.restoreOpenState()
+        }
+    }
+
+    disconnect() {
+        document.removeEventListener('keydown', this.handleKeyDown)
+    }
+
+    // Przywraca stan otwartego sidebara bez animacji
+    restoreOpenState() {
+        this.sidebarTarget.classList.remove('hidden')
+        document.body.classList.add('overflow-hidden')
+
+        this.backdropTarget.classList.remove('opacity-0')
+        this.backdropTarget.classList.add('opacity-100')
+
+        this.panelTarget.classList.remove('-translate-x-full')
+        this.panelTarget.classList.add('translate-x-0')
+
+        this.closeBtnTarget.classList.remove('opacity-0')
+        this.closeBtnTarget.classList.add('opacity-100')
+
+        this.isOpen = true
+    }
+
     // Obsługa otwierania sidebara
     open() {
         if (this.isOpen) return
 
         // Pokazujemy container (ale jeszcze niewidoczne elementy)
         this.sidebarTarget.classList.remove('hidden')
+
+        // Zapisz stan do localStorage
+        localStorage.setItem('sidebarOpen', 'true')
 
         // Dajemy czas na zaaplikowanie klas przed animacją
         requestAnimationFrame(() => {
@@ -41,6 +78,9 @@ export default class extends Controller {
     close() {
         if (!this.isOpen) return
 
+        // Zapisz stan do localStorage
+        localStorage.setItem('sidebarOpen', 'false')
+
         // Cofamy animacje
         this.backdropTarget.classList.remove('opacity-100')
         this.backdropTarget.classList.add('opacity-0')
@@ -64,18 +104,7 @@ export default class extends Controller {
         this.isOpen = false
     }
 
-    // Dodatkowe metody pomocnicze
-
     // Zamyka sidebar po naciśnięciu klawisza Escape
-    disconnect() {
-        document.removeEventListener('keydown', this.handleKeyDown)
-    }
-
-    connect() {
-        this.handleKeyDown = this.handleKeyDown.bind(this)
-        document.addEventListener('keydown', this.handleKeyDown)
-    }
-
     handleKeyDown(event) {
         if (event.key === 'Escape' && this.isOpen) {
             this.close()
