fabc8855e83a8e7784700013f5247869a90f5e72
Oskar Grabowski <oskar.grabowski@gmail.com>
2025-06-07 16:08:56 +0200

    About pages

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,10 @@ Rails.application.routes.draw do
     resources :messages, only: [ :create ]
   end
 
+  get "about_lina", to: "details#about_lina"
+  get "about_dev", to: "details#about_dev"
+  get "profile", to: "details#profile"
+
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
