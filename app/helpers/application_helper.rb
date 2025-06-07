3207e8cde6ed9373d5ef946742d0b5785fd70a16
Oskar Grabowski <oskar.grabowski@gmail.com>
2025-06-07 16:49:35 +0200

    Messages partials and UI fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def bot_avatar(size: "size-10")
+    image_tag "lina_avatar.png", alt: "Lina avatar", class: "#{size} rounded-full object-cover"
+  end
+
+  def dev_avatar(size: "size-16")
+    image_tag "dev_avatar.png", alt: "Dev avatar", class: "#{size} rounded-full object-cover"
+  end
 end
