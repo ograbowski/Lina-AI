0cdc85d4ba1ac93992c790de0bd90152563eb5fe
Oskar Grabowski <oskar.grabowski@gmail.com>
2025-06-08 15:26:25 +0200

    responsive design & user avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,13 @@
 module ApplicationHelper
   def bot_avatar(size: "size-10")
-    image_tag "lina_avatar.png", alt: "Lina avatar", class: "#{size} rounded-full object-cover"
+    image_tag "lina_avatar.png", alt: "Lina avatar", class: "#{size} rounded-full object-cover flex-shrink-0"
   end
 
   def dev_avatar(size: "size-16")
-    image_tag "dev_avatar.png", alt: "Dev avatar", class: "#{size} rounded-full object-cover"
+    image_tag "dev_avatar.png", alt: "Dev avatar", class: "#{size} rounded-full object-cover flex-shrink-0"
+  end
+
+  def user_avatar(size: "size-10")
+    image_tag "user_avatar.png", alt: "User avatar", class: "#{size} rounded-full object-cover flex-shrink-0"
   end
 end
