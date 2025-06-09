f63f26b0bdf637718faf4ca71be684843a9e637d
Oskar Grabowski <oskar.grabowski@gmail.com>
2025-06-09 17:25:32 +0200

    Layout adjustments to sidebar

--- app/javascript/controllers/sidebar_controller.js
+++ app/javascript/controllers/sidebar_controller.js
@@ -1,43 +1,85 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-    static targets = [ "sidebar", "panel", "closeBtn" ]
+    static targets = [ "sidebar", "panel", "closeBtn", "mainContent", "openBtn" ]
 
     initialize() {
         this.isOpen = false
+        this.sidebarWidth = 224 // w-56 = 224px
     }
 
     connect() {
         this.handleKeyDown = this.handleKeyDown.bind(this)
         document.addEventListener('keydown', this.handleKeyDown)
 
-        const savedState = localStorage.getItem('sidebarOpen')
-        if (savedState === 'true') {
-            this.restoreOpenState()
-        }
+        requestAnimationFrame(() => {
+            this.initializeSidebarState()
+        })
+
+        // Nasłuchuj na Turbo events
+        document.addEventListener('turbo:load', () => {
+            requestAnimationFrame(() => {
+                this.initializeSidebarState()
+            })
+        })
+
+        document.addEventListener('turbo:render', () => {
+            requestAnimationFrame(() => {
+                this.initializeSidebarState()
+            })
+        })
     }
 
     disconnect() {
         document.removeEventListener('keydown', this.handleKeyDown)
     }
 
-    restoreOpenState() {
-        this.sidebarTarget.classList.remove('hidden')
+    initializeSidebarState() {
+        const savedState = localStorage.getItem('sidebarOpen')
 
+        if (savedState === 'true') {
+            this.restoreOpenState()
+        } else {
+            this.ensureClosedState()
+        }
+    }
+
+    restoreOpenState() {
+        if (!this.hasSidebarTarget || !this.hasPanelTarget || !this.hasCloseBtnTarget) {
+            return
+        }
+
+        this.sidebarTarget.classList.remove('hidden')
         this.panelTarget.classList.remove('-translate-x-full')
         this.panelTarget.classList.add('translate-x-0')
-
         this.closeBtnTarget.classList.remove('opacity-0')
         this.closeBtnTarget.classList.add('opacity-100')
 
         this.isOpen = true
+        this.adjustMainContentMargin()
+        this.updateButtonVisibility()
+    }
+
+    ensureClosedState() {
+        if (!this.hasSidebarTarget || !this.hasPanelTarget || !this.hasCloseBtnTarget) {
+            return
+        }
+
+        this.sidebarTarget.classList.add('hidden')
+        this.panelTarget.classList.add('-translate-x-full')
+        this.panelTarget.classList.remove('translate-x-0')
+        this.closeBtnTarget.classList.add('opacity-0')
+        this.closeBtnTarget.classList.remove('opacity-100')
+
+        this.isOpen = false
+        this.adjustMainContentMargin()
+        this.updateButtonVisibility()
     }
 
     open() {
-        if (this.isOpen) return
+        if (this.isOpen || !this.hasSidebarTarget) return
 
         this.sidebarTarget.classList.remove('hidden')
-
         localStorage.setItem('sidebarOpen', 'true')
 
         requestAnimationFrame(() => {
@@ -48,14 +90,13 @@ export default class extends Controller {
             this.closeBtnTarget.classList.add('opacity-100')
 
             this.isOpen = true
+            this.adjustMainContentMargin()
+            this.updateButtonVisibility()
         })
     }
 
     close() {
-        const isVisuallyOpen = !this.sidebarTarget.classList.contains('hidden') &&
-            this.panelTarget.classList.contains('translate-x-0')
-
-        if (!this.isOpen && !isVisuallyOpen) return
+        if (!this.isOpen || !this.hasSidebarTarget) return
 
         localStorage.setItem('sidebarOpen', 'false')
 
@@ -72,6 +113,34 @@ export default class extends Controller {
         }, 300)
 
         this.isOpen = false
+        this.adjustMainContentMargin()
+        this.updateButtonVisibility()
+    }
+
+    updateButtonVisibility() {
+        if (this.hasOpenBtnTarget) {
+            if (this.isOpen) {
+                // Ukryj przycisk open gdy sidebar jest otwarty
+                this.openBtnTarget.classList.add('opacity-0', 'pointer-events-none', 'disabled')
+                this.openBtnTarget.classList.remove('opacity-100')
+            } else {
+                // Pokaż przycisk open gdy sidebar jest zamknięty
+                this.openBtnTarget.classList.remove('opacity-0', 'pointer-events-none', 'disabled')
+                this.openBtnTarget.classList.add('opacity-100')
+            }
+        }
+    }
+
+
+
+    adjustMainContentMargin() {
+        if (this.hasMainContentTarget) {
+            if (this.isOpen) {
+                this.mainContentTarget.style.marginLeft = `${this.sidebarWidth}px`
+            } else {
+                this.mainContentTarget.style.marginLeft = '0px'
+            }
+        }
     }
 
     handleKeyDown(event) {
